70cabafe5295449905a840b80822aad6285730af
mrwerner392 <mrwerner392@gmail.com>
2019-08-19 15:16:08 -0400

    brewery class - sold beer count

--- lib/brewery.rb
+++ lib/brewery.rb
@@ -1,8 +1,18 @@
 class Brewery < ActiveRecord::Base
     has_many :beers
 
+    def sold_beer_count
+        # returns the number of beers bought for each of the brewery's beer instances
+        count_hash = {}
+        beers.each do |beer|
+            count_hash[beer] = (ConsumerBeer.all.where(beer_id: beer.id).sum(:num_consumed) + ConsumerBeer.all.where(beer_id: beer.id).sum(:num_available))
+        end
+        count_hash
+    end
+
     def most_popular
         #returns the beer instance of the brewery's most bought beer
+        # self.bought_beer_count.sort_by {|beer, num_bought| num_bought}.last
     end
 
     def least_popular
