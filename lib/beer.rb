a5af7d5b71c9b0e3d35cea63785ac32858462531
shanelonergan <sptlonergan@gmail.com>
2019-08-19 15:16:00 -0400

    updated beer methods

--- lib/beer.rb
+++ lib/beer.rb
@@ -3,10 +3,16 @@ class Beer < ActiveRecord::Base
     has_many :consumer_beers
     has_many :consumers, through: :consumer_beers
 
+    def
+
     def average_rating
-        #returns the average rating of all user_beer instances of this beer
+        #returns the average rating of all consumer_beer instances of this beer
+        consumer_beers_with_ratings = self.consumer_beers.select(rating: true)
+        average_rating = consumer_beers_with_ratings.average(:rating)
+        #make it legible
+        average_rating.to_f.floor(2) 
     end
-    
+
     def self.most_popular
         #returns the instance of the most bought beer
     end
